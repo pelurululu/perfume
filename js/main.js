dd744b37a6b2226d850620a223b39769dabdf957
pelurululu <muhdzulkml144@gmail.com>
2026-04-01 15:42:53 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 async function init() {
   initAnnouncementCountdown();
   initScrollReveal();
-  // Load pricing first (affects price display in cards)
   await loadPricing();
-  // Load + render products
   await loadProducts();
-  // Init stock from loaded products
   initStock();
-  // Restore cart
   refreshCartUI();
   openPromoPopup();
 }
+
 init();
